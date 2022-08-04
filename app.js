53310539243c14fe645c9f34a902916f592c2337
YaroslavOnofriichuk <YaroslavOnofriichuk@ukr.net>
2022-08-04 15:30:12 +0300

    change cors

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
-// app.use(
-//   cors({
-//     origin: ["https://job-search-statistics.netlify.app"],
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://job-search-statistics.netlify.app"],
+    credentials: true,
+  })
+);
+// app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/notes", notesRouter);
