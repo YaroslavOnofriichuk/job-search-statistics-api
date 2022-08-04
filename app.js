d57b9d4eae12d9e34eb5e3b3fdb8c390c5275bf9
YaroslavOnofriichuk <YaroslavOnofriichuk@ukr.net>
2022-08-04 16:25:53 +0300

    change cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
     "https://job-search-statistics.netlify.app"
   );
   res.header(
-    "Access-Control-Allow-Headera",
+    "Access-Control-Allow-Headers",
     "origin, content-type, accept, Authorization, X-Requested-With"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
