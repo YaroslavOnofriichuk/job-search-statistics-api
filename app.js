050a100e47d450d850c789ad2e0c9bb098f3a80f
YaroslavOnofriichuk <YaroslavOnofriichuk@ukr.net>
2022-08-05 10:55:26 +0300

    change cors settings

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
-// const cors = require("cors");
-const { cors } = require("./middlewares");
+const cors = require("cors");
+// const { cors } = require("./middlewares");
 
 const { notesRouter, authRouter, usersRouter } = require("./routes/api");
 
