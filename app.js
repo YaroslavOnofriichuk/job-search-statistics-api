52131855c5bac618755240ef18925ddf86d825c4
YaroslavOnofriichuk <YaroslavOnofriichuk@ukr.net>
2022-08-06 11:14:28 +0300

    add cors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 // const cors = require("cors");
-const { cors } = require("./middlewares");
+// const { cors } = require("./middlewares");
 
 const { notesRouter, authRouter, usersRouter } = require("./routes/api");
 
@@ -11,7 +11,24 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 // app.use(cors({ origin: "https://job-search-statistics.netlify.app" }));
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  next();
+});
 app.use(express.json());
 
 app.use("/api/v1/notes", notesRouter);
